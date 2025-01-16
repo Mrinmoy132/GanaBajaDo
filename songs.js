12340f41c70279439bdd65d6a59493278fb54f40
Mrinmoy132 <83785901+Mrinmoy132@users.noreply.github.com>
2025-01-16 17:54:59 +0530

    Update songs.js

--- songs.js
+++ songs.js
@@ -96,7 +96,7 @@
 
 
 async function getsongs() {
-    let songs = await fetch("musics/");  //fetching data from the server/filesystem
+    let songs = await fetch("./");  //fetching data from the server/filesystem
     let response = await songs.text();                                              //converting the songs to html text format
     let newdiv = document.createElement("div");
     newdiv.innerHTML = response;                                                    //html text to html
