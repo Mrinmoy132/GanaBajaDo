11d86552080b382c8d8effcb0139c95c066d55ef
Mrinmoy132 <83785901+Mrinmoy132@users.noreply.github.com>
2025-01-17 12:10:54 +0530

    Update songs.js

--- songs.js
+++ songs.js
@@ -96,14 +96,14 @@ language_changing_innerdiv.addEventListener('mouseout', () => {
 
 
 async function getsongs() {
-    let songs = await fetch("musics/");  //fetching data from the server/filesystem
+    let songs = await fetch("/musics/");  //fetching data from the server/filesystem
     let response = await songs.text();  //converting the songs to html text format
     let newdiv = document.createElement("div");
     newdiv.innerHTML = response;                                                    //html text to html
     let a = newdiv.getElementsByTagName("a");                                       //filtering all the <a> elements from html 
     const songs_arr = [];
     for (const element of a) {
-        if (element.href.endsWith(".jpg")) {                                        //all the elements ended with .mp3
+        if (element.href.endsWith(".jpg") && !e.href.includes(".htaccess"))) {                                        //all the elements ended with .mp3
             songs_arr.push(element.href);
         }
     }
